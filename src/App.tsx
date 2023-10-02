fb7919dcc2c08044f9c1d2d89f48af6b172acafc
Augusto Ávila <augusto.avila@ctw.bmwgroup.com>
2023-10-02 22:26:41 +0100

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+
+import { Router } from './Router'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
   return (
-    <h1>Hello</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
